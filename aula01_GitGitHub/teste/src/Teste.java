c9d63bcfa0be8104d69f6102513127cda950acaf
Geisiane Silva <geisysilva2212@gmail.com>
2024-03-14 10:57:10 -0300

    corrigindo o codigo

--- aula01_GitGitHub/teste/src/Teste.java
+++ aula01_GitGitHub/teste/src/Teste.java
@@ -1,7 +1,7 @@
-package teste;
+//package teste;
 
 public class Teste {
-  publlic static void main(String[] args) {
+  public static void main(String[] args) {
     int num1 = 3, num2= 5, soma;
 
           soma = num1 + num2;
