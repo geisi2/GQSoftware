21ada00f472d888bb5676aeb8661b6b7430b6036
Geisiane Silva Jeronimo <geisysilva2212@gmail.com>
2024-04-15 20:22:13 -0300

    add descrição

---
+++ aula02_GitGitHub/teste/src/Teste.java
@@ -0,0 +1,10 @@
+//package teste;
+
+public class Teste {
+  public static void main(String[] args) {
+    int num1 = 3, num2= 5, soma;
+
+          soma = num1 + num2;
+         System.out.println(" A soma dos números "  + num1 + " e " + num2 +" é igual a " + soma);
+  }
+}
